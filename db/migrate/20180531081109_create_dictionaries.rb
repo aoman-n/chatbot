50d9fdcc4fb450976165555dc3553bbe14940776
first commit <>
2018-05-31 18:13:56 +0900

    dictionary model create

---
+++ db/migrate/20180531081109_create_dictionaries.rb
@@ -0,0 +1,8 @@
+class CreateDictionaries < ActiveRecord::Migration[5.0]
+  def change
+    create_table :dictionaries do |t|
+      t.text        :response, null: false
+      t.text        :request, null: false
+    end
+  end
+end

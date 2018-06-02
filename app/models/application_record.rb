662901c6c26e035317b3e3b00dc69a6766b87758
first commit <>
2018-06-02 18:03:53 +0900

    time zone setting

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def created_date
+    created_at.strftime("%Y-%m-%dT%H:%M:%S")
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Chatbot
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.

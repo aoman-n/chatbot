24375779256e7b0accc23549cc4f5f770e38a0df
first commit <>
2018-06-07 11:43:06 +0900

    routing set dictionaries index create

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to:                'chats#index'
   post '/chat'         => 'chats#create'
   get  '/history/list' => 'histories#index'
+  resources :dictionaries, only: [:index, :create]
 end

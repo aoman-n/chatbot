9fc8f5aacd2897f0f4feeb884ea026fe4c5507d9
first commit <>
2018-06-07 11:47:23 +0900

    dictionaries_controller index & craete action

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -1,2 +1,15 @@
 class DictionariesController < ApplicationController
+  def index
+    @dictionary = Dictionary.new
+  end
+
+  def create
+    @dictionary = Dictionary.new(dictionary_params)
+    redirect_to action: :index unless @dictionary.save
+  end
+
+  private
+  def dictionary_params
+    params.require(:dictionary).permit(:request, :response)
+  end
 end
